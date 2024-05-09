c3176100ea44943e99fe0cb34e9d3d3873f9e3cc
andewq121 <168224103+andewq121@users.noreply.github.com>
2024-05-09 11:31:05 +0300

    Update 2nd_step.sh

--- 2nd_step.sh
+++ 2nd_step.sh
@@ -2,5 +2,5 @@ opkg install python3 python3-pip make
 +pip install -r requirements.txt
 read -p 'fw version ' fw
 make -C stage1 FW=$fw clean && make -C stage1 FW=$fw
-make -C stage2 $fw clean && make -C stage2 FW=$fw
+make -C stage2 FW=$fw clean && make -C stage2 FW=$fw
 python3 pppwn.py --interface=lan --fw=$fw
